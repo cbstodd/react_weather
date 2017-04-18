7cf018ec123b8bc90f0e97f4ac1b6b02fbd930c9
cbstodd <cbstodd@gmail.com>
2017-04-18 14:43:53 -0400

    fixed server to be used for production on heroku

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var express = require('express');
+const express = require('express');
 
 // Create our app
-var app = express();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Makes sure traffic is on HTTP
+app.use(function( req, resp, next ){
+    if (req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    } else {
+        resp.redirect(`http://${req.hostname}${req.url}`);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('### Express server is up on http://localhost:3000 ###');
+app.listen(PORT, function(){
+    console.log(`### Express server listening on port ${PORT} ###`);
 });
