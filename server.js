e9f5bb18507266a7b44790498ba19f2606c8b732
cbstodd <cbstodd@gmail.com>
2017-04-22 21:57:53 -0400

    fix server on heroku

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Makes sure traffic is on HTTP
-// app.use(function( req, resp, next ){
-//     if (req.headers['x-forwarded-proto'] === 'https'){
-//         resp.redirect(`http://${req.hostname}${req.url}`);
-//     } else {
-//         next();
-//     }
-// });
+app.use(function( req, resp, next ){
+    if (req.headers['x-forwarded-proto'] === 'https'){
+        resp.redirect(`http://${req.hostname}${req.url}`);
+    } else {
+        next();
+    }
+});
 
 app.use(express.static('public'));
 
