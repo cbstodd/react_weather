4bde2fef6eff912933cc435758b4690d69bb526f
cbstodd <cbstodd@gmail.com>
2017-04-22 21:53:36 -0400

    added sass

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Makes sure traffic is on HTTP
-app.use(function( req, resp, next ){
-    if (req.headers['x-forwarded-proto'] === 'https'){
-        resp.redirect('http://$'+ req.hostname + ''+ req.url);
-
-    } else {
-        next();
-    }
-});
+// app.use(function( req, resp, next ){
+//     if (req.headers['x-forwarded-proto'] === 'https'){
+//         resp.redirect(`http://${req.hostname}${req.url}`);
+//     } else {
+//         next();
+//     }
+// });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
-    console.log('### Express server listening on port ' + PORT + '###');
+    console.log(`### Express server listening on http://localhost:${PORT} ###`);
 });
